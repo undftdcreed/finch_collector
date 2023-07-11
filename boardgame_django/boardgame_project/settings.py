7c9e671cc9dbfb861dfdb4d75588b78f2fa4dcd2
Brandonbay <undftdcreed@gmail.com>
2023-07-11 14:41:47 -0700

    fixed new bug

--- boardgame_django/boardgame_project/settings.py
+++ boardgame_django/boardgame_project/settings.py
@@ -146,8 +146,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
