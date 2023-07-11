d506a775333d7ef88f326f550ba3d5286caf43cb
Brandonbay <undftdcreed@gmail.com>
2023-07-11 15:03:16 -0700

    whitenoise

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 pip install django
 pip install dj_database_url
 pip install psycopg2-binary
+pip install whitenoise
 python manage.py collectstatic --no-input
 python manage.py migrate
