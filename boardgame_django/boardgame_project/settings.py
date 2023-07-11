329d4cc3b1894c9dce1653489da0f4ed60caa200
Brandonbay <undftdcreed@gmail.com>
2023-07-11 10:15:52 -0700

    new changes

--- boardgame_django/boardgame_project/settings.py
+++ boardgame_django/boardgame_project/settings.py
@@ -40,7 +40,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'render.apps.config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
